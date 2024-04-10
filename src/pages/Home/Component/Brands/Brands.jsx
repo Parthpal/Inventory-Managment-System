b3d7ed57113bdc5163207df6f95019a28a5c9e3e
Parthpal <parthapradippal@gmail.com>
2024-04-11 01:17:28 +0530

    Brands added

---
+++ src/pages/Home/Component/Brands/Brands.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Marquee from 'react-fast-marquee';
+
+const Brands = () => {
+    return (
+        <div>
+            <h2 className='text-center text-4xl font-bold my-4'>Brands Using Our Products </h2>
+            <Marquee className='' autoFill={true} pauseOnHover={true} speed={30}>
+                <img className='w-32 h-28 mx-2' src='https://i.ibb.co/3YskX97/logo-2.jpg'></img>
+                <img className='w-32 h-28 mx-2' src='https://i.ibb.co/hgqgjRS/logo-3.jpg'></img>
+                <img className='w-32 h-28 mx-2' src='https://i.ibb.co/YT9cB08/logo-4.jpg'></img>
+                <img className='w-32 h-28 mx-2' src='https://i.ibb.co/MMLhjh4/logo-5.jpg'></img>
+                <img className='w-32 h-28 mx-2' src='https://i.ibb.co/dJGDYHs/logo-6.jpg'></img>
+                <img className='w-32 h-28 mx-2' src='https://i.ibb.co/YT9cB08/logo-4.jpg'></img>
+            </Marquee>
+        </div>
+    );
+};
+
+export default Brands;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './Component/Navbar/Navbar';
 import Banner from './Component/Banner/Banner';
+import Brands from './Component/Brands/Brands';
 
 const Home = () => {
     return (
         <div>
             <Navbar/>
             <Banner/>
+            <Brands/>
         </div>
     );
 };
